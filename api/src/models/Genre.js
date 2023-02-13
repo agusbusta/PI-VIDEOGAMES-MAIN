0d866e3c3e89bdfe2ffedaa3577b65dbb41cad5b
Agustin <agusbus95@gmail.com>
2023-02-13 12:07:33 -0300

    model genre created

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -1,18 +1,25 @@
+//<--------IMPORTACIONES--------->
+
 const { DataTypes } = require('sequelize');
+
+//<------------------------------
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
 
-const Genre = sequelize.define('genre', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
-}
+	// defino el modelo
 
+	sequelize.define('genre', {
+
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
+};
