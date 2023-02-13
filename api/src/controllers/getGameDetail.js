6a7c622f424621272a4cab69d251be3bd6e5daf4
Agustin <agusbus95@gmail.com>
2023-02-12 21:41:11 -0300

    getGameDetail controller created

---
+++ api/src/controllers/getGameDetail.js
@@ -0,0 +1,45 @@
+//<-----------IMPORTACIONES------------>
+require('dotenv').config();
+const axios = require('axios');
+const { API_KEY } = process.env;
+//------------------------------------>
+
+//<-----------TRAER DETALLE DE JUEGO POR ID DESDE API------------>
+const getDetailGame = async (idGame) => {
+	let gamesApi = await axios.get(
+		`https://api.rawg.io/api/games/${idGame}?key=${API_KEY}`
+	);
+
+	const {
+		id,
+		name,
+		background_image: image,
+		rating,
+		released,
+		description_raw: description,
+		platforms,
+		genres,
+	} = gamesApi.data;
+
+	const game = {
+		id,
+		name,
+		image,
+		rating,
+		released,
+		description,
+		platforms: platforms ? platforms.map((x) => x.platform.name) : null,
+		genres: genres.map((e) => e.name),
+		createdInDb: false,
+	};
+
+	return game;
+};
+
+//-------------------------------------->
+
+//<-----------EXPORTACIONES------------>
+
+module.exports = getDetailGame;
+
+//-------------------------------------->
