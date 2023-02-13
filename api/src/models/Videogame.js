bdfaa13df89be51fd43f578c1813b1ff0938a45a
Agustin <agusbus95@gmail.com>
2023-02-13 16:48:57 -0300

    videogame model created

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,38 +1,66 @@
-const { DataTypes } = require('sequelize');
+//<----------IMPORTACIONES Y REQUERIMIENTOS------------>
+
+const { DataTypes, UUIDV4 } = require('sequelize');
+
+//<----------------------------------------------------
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
+
   // defino el modelo
+
   sequelize.define('videogame', {
+
     id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: UUIDV4,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    platforms: {
-      type: DataTypes.ARRAY(sequelize.STRING),
-      allowNull: false
-    },
+    },  
     image: {
       type: DataTypes.STRING,
-      allowNull: false
+      validate: {
+        isUrl: {
+          msg: 'url format: https://foo.com',
+        },
+      },
     },
-    releaseDate: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    rating: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    }
-    
-  });
+    description: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			released: {
+				type: DataTypes.DATEONLY,
+				validate: {
+					isDate: {
+						msg: 'date format: YYYY-MM-DD',
+					},
+				},
+			},
+			rating: {
+				type: DataTypes.FLOAT,
+				validate: {
+					min: 0,
+					max: 5,
+				},
+			},
+			platforms: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+			},
+			createdInDb: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+				defaultValue: true,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
 };
